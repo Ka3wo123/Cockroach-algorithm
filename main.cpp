3251dbd20f809b1b86c2b924e95e1a350fb0e2d4
Kajetan Nowak <kajtek2n@gmail.com>
2023-03-22 12:16:32 +0100

    Created 3 test functions

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+
+#define PI 3.14159265
 
 using namespace std;
 
+double hiperElipsoide(vector<double> x, int dim) {
+    double sum = 0.0;
+    for(int i = 1; i <= dim; i++) {
+        sum += i * (x[i-1] * x[i-1]);
+    }
+    return sum;
+}
+
+double rastring(vector<double> x, int dim) {
+    double sum = 0.0;
+    for(int i = 1; i <= dim; i++) {
+        sum += (x[i-1] * x[i-1]) - 10 * cos(PI * 2 * x[i-1]);
+    }
+
+    return 10 * dim + sum;
+}
+
+double schwefel(vector<double> x, int dim) {
+    double sum = 0.0;
+    for(int i = 1; i <= dim; i++) {
+        sum += -x[i-1] * sin(sqrt(fabs(x[i-1])));
+    }
+
+    return -sum;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+
 }
