3d26ae0d2db1d244d73e62aebf029bf6fff7d65b
Kajetan Nowak <kajtek2n@gmail.com>
2023-03-22 13:08:06 +0100

    Changes not completed

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <random>
+#include <ctime>
 
 #define PI 3.14159265
 
@@ -23,16 +25,40 @@ double rastring(vector<double> x, int dim) {
     return 10 * dim + sum;
 }
 
-double schwefel(vector<double> x, int dim) {
+double schwefel(double x, int dim) {
     double sum = 0.0;
     for(int i = 1; i <= dim; i++) {
-        sum += -x[i-1] * sin(sqrt(fabs(x[i-1])));
+        sum += -x * sin(sqrt(fabs(x)));
     }
 
     return -sum;
 }
 
+vector<double> updatePosition() {
+
+}
+
+vector<double> findGlobalBest(vector<double> cockroaches, int dim, double (*func)(double)) {
+    vector<double> globalBest;
+    globalBest[0] = cockroaches[0];
+    for(int i = 1; i < dim; i++) {
+        if(func(cockroaches[i]) < func(globalBest[0])) {
+            globalBest[0] = cockroaches[i];
+        }
+    }
+
+    return globalBest;
+}
+
+vector<double> cockroachAlgorithm(int numCockroaches, int maxIter, double eps, double lowerBound, double upperBound) {
+    vector<double> cockroachesPosition(numCockroaches);
+}
+
+
+
 int main()
 {
+
+
 
 }
