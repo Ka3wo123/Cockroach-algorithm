3d78caff19901f5b7870724f347f43e969a0e4b6
Kajetan Nowak <kajtek2n@gmail.com>
2023-03-24 11:45:00 +0100

    Rastring, hiper elipsoide works but schwefel needs to be corrected as function

--- main.cpp
+++ main.cpp
@@ -28,10 +28,10 @@ double rastring(vector<double> x, int dim) {
 double schwefel(vector<double> x, int dim) {
     double sum = 0.0;
     for(int i = 1; i <= dim; i++) {
-        sum += -x[i-1] * sin(sqrt(fabs(x[i-1])));
+        sum += x[i-1] * sin(sqrt(fabs(x[i-1])));
     }
 
-    return -sum;
+    return 418.9829 * dim - sum;
 }
 
 
@@ -56,60 +56,109 @@ vector<double> findGlobalOptimum(vector<vector<double>> cockroaches, double (*te
     globalOptimum = cockroaches[0];
 
     for(int i = 1; i < cockroaches.size(); i++) {
-        if(testFunction(cockroaches[i], dim) < testFunction(globalOptimum, dim)) {
+        if(testFunction(cockroaches[i], dim) < testFunction(globalOptimum, dim)) {            
             globalOptimum = cockroaches[i];
-        }
+        }        
     }
 
 
     return globalOptimum;
 }
 
+vector<double> updatePosition(vector<double> position, vector<double> optimumPosition, double stepSize) {
+    random_device rd;
+    mt19937 random(rd());
+    uniform_real_distribution<> dis{0, 1};
+    vector<double> newPosition(position.size());
+
+    for (int i = 0; i < position.size(); i++) {
+        double rand = dis(random);
+        double delta = rand * (optimumPosition[i] - position[i]);
+        newPosition[i] = position[i] + stepSize * delta;
+    }
+    return newPosition;
+}
+
+bool isLocalOptimum(vector<double> localOpt, vector<double> pos) {
+    for(int i = 0; i < localOpt.size(); i++) {
+        if (localOpt[i] != pos[i]) return false;
+    }
+
+    return true;
+}
+
+double diffCocks(vector<double> cockI, vector<double> cockJ) {
+    double sum = 0;
+
+    for(int i = 0; i < cockI.size(); i++) {
+        sum += fabs(cockI[i] - cockJ[i]) * fabs(cockI[i] - cockJ[i]);
+    }
+
+    return sqrt(sum);
+}
+vector<double> updateInLightIter(vector<double> pos) {
+    random_device rd;
+    mt19937 random(rd());
+    uniform_real_distribution<> dis(0, 1);
+
+    vector<double> newPos;
+
+    for(int i = 0; i < pos.size(); i++) {
+        newPos.push_back(pos[i] + dis(random));
+    }
+
+    return newPos;
+}
+
 
 vector<double> cockroachAlgorithm(int numCockroach, int dim, int maxIter, double lowerBound, double upperBound, double visual, double eps, double w, double (*testFunction)(vector<double>, int)) {
     vector<double> globalOptimum;
     vector<double> localOptimum;
-    vector<vector<double>> cockroaches(numCockroach);
+    vector<vector<double>> cockroaches;
+    random_device rd;
+    mt19937 random(rd());
+    uniform_real_distribution<> dis(1, (double) numCockroach);
+    uniform_int_distribution<> light(1, maxIter);
+
+    int lightIteration;
 
     for (int i = 0; i < numCockroach; i++) {
-        cockroaches[i] = generateRandomSolution(dim, lowerBound, upperBound);
+        cockroaches.push_back(generateRandomSolution(dim, lowerBound, upperBound));
     }
 
     globalOptimum = findGlobalOptimum(cockroaches, testFunction);
 
 
-    for(int t = 0; t < maxIter; t++) {
+    for(int t = 0; t < maxIter; t++) {        
         if(testFunction(globalOptimum, dim) <= eps) {
             return globalOptimum;
         }
-        for(int i = 0; i < numCockroach; i++) {
+
+        lightIteration = light(random);
+
+        for(int i = 0; i < numCockroach; i++) {            
             for(int j = 0; j < numCockroach; j++) {
-                if((fabs(cockroaches[t][i] - cockroaches[t][j]) < visual) && (testFunction(cockroaches[i], dim) < testFunction(cockroaches[j], dim))) {
+                if(diffCocks(cockroaches[i], cockroaches[j]) < visual && (testFunction(cockroaches[j], dim) < testFunction(cockroaches[i], dim))) {
                     localOptimum = cockroaches[j];
                 }
             }
 
-            if(localOptimum[i] == cockroaches[t][i]) {
-                cockroaches[t][i] = w * cockroaches[t][i] + i + rand() % 2 * (globalOptimum[i] - cockroaches[t][i]);
-            } else {
-                cockroaches[t][i] = w * cockroaches[t][i] + i + rand() % 2 * (localOptimum[i] - cockroaches[t][i]);
+            if(isLocalOptimum(localOptimum, cockroaches[i])) {
+                cockroaches[i] = updatePosition(cockroaches[i], globalOptimum, w);
+            } else {                
+                cockroaches[i] = updatePosition(cockroaches[i], localOptimum, w);
             }
 
             if(testFunction(cockroaches[i], dim) < testFunction(globalOptimum, dim)) {
                 globalOptimum = cockroaches[i];
-            }
+            }           
+        }                
 
-            cout << "Global optimum: ";
-            for(int wypisz = 0; wypisz < globalOptimum.size(); wypisz++) {
-                cout << globalOptimum[wypisz] << " ";
-            }
-            cout << endl;
-        }
-
-        if(t == 4 || t == 10 || t == 16 || t == 50 || t == 89) {
+        if(t == lightIteration) {
 
             for(int i = 0; i < numCockroach; i++) {
-                cockroaches[t][i] = cockroaches[t][i] + rand() % 2 + 1;
+                cockroaches[i] = updateInLightIter(cockroaches[i]);
+
 
                 if(testFunction(cockroaches[i], dim) < testFunction(globalOptimum, dim)) {
                     globalOptimum = cockroaches[i];
@@ -117,11 +166,17 @@ vector<double> cockroachAlgorithm(int numCockroach, int dim, int maxIter, double
             }
         }
 
-        int k = rand() % numCockroach + 1;
+        int k = dis(random);
 
-        if(cockroaches[k] != globalOptimum) {
+        if(!isLocalOptimum(globalOptimum, cockroaches[k])) {
             cockroaches[k] = globalOptimum;
         }
+
+        cout << "Iteration " << t << " ";
+        for(int i = 0; i < globalOptimum.size(); i++) {
+            cout << globalOptimum[i] << " ";
+        }
+        cout << endl;
     }
 
     return globalOptimum;
@@ -132,24 +187,33 @@ vector<double> cockroachAlgorithm(int numCockroach, int dim, int maxIter, double
 int main()
 {
 
-    const int NUM_OF_CROCKROACHES = 700;
-    const int MAX_ITER = 2000;
+    const int NUM_OF_CROCKROACHES = 250;
+    const int MAX_ITER = 300;
     const double EPS = 0.001;
-    const double VISUAL = 0.5;
+    const double VISUAL = 0.2;
     const int DIM = 2;
     const double W = 0.1;
     const double LOWER_BOUND = -500;
     const double UPPER_BOUND = 500;
 
-    vector<double> solution = cockroachAlgorithm(NUM_OF_CROCKROACHES, DIM, MAX_ITER, LOWER_BOUND, UPPER_BOUND, VISUAL, EPS, W, &schwefel);
+    vector<double> solution = cockroachAlgorithm(NUM_OF_CROCKROACHES, DIM, MAX_ITER, LOWER_BOUND, UPPER_BOUND, VISUAL, EPS, W, &rastring);
 
 
     cout << "Znaleziono rozwiazanie: " << endl;
-    cout << "F(x) = " << schwefel(solution, DIM) << " dla x { ";
+    cout << "F(x) = " << rastring(solution, DIM) << " dla x { ";
     for(auto x : solution) {
         cout << x << " ";
     }
 
     cout << "}" << endl;
+
+
+
+
+
+
+
+
+
 
 }
